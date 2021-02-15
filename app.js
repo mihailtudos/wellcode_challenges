967f972252bcf811e529a3de767f0582cc3bc69e
Mihail Tudos <mihailtudos@outlook.com>
2021-02-15 14:11:58 +0000

    changed the interface and game logic

--- app.js
+++ app.js
@@ -2,34 +2,43 @@ const buttonStart = document.getElementById('number');
 const userInputError = document.getElementById('numberError');
 const playGround = document.getElementById('playGround');
 const submitButton = document.getElementById('submitButton');
-let hiddenNumber;
+const playerName = document.getElementById('playerName');
+const playerDetails = document.getElementById('player');
+const gameDetails = document.getElementsByClassName('gameDetails');
+const choiceFeedback = document.getElementById('choiceFeedback');
+const lifes = document.querySelectorAll('#gamePlayerDetails i');
+
+let hiddenNumber, count = 0;
 
 document.getElementById("submitButton").addEventListener("click", function(event){
     event.preventDefault()
   });
 
+function removeLife(i) {
+    lifes[i].style.color = 'red';
+}
+
 const checkWinner = function() {
-    const magic_number = Math.floor(Math.random() * hiddenNumber);
-    console.log(magic_number);
-    console.log(this.id);
-    if (parseInt(this.id) == magic_number) {
-        let startNewGame = confirm('Congratulations you won! Do you want to play more?');
-        if (!startNewGame) {
-            location.reload();
-        }
-    } else {
-        let startNewGame = confirm('You\'ve lost this time! Do you want to play more?');
-        if (!startNewGame) {
-            location.reload();
-        }
+    if(count < 3) {
+        const magic_number = Math.floor(Math.random() * hiddenNumber);
+        console.log(this.id, magic_number);
+        removeLife(count);
+        count++;
+    } 
+    
+    if (count === 3) {
+        modal.style.display = "block";
     }
 }
 
 function playGame() {
-    let numberOfButtons = buttonStart.value;
-    if (numberOfButtons > 1 && numberOfButtons <= 10) {
+    const numberOfButtons = buttonStart.value;
+    const name = playerName.value;
+    if (numberOfButtons > 1 && numberOfButtons <= 10 && name !== '') {
         submitButton.disabled = true;
         hiddenNumber = numberOfButtons;
+        playerDetails.innerHTML = `Player: ${playerName.value}`;
+        gameDetails[0].style.display = 'flex';
         for (let i = 0; i < numberOfButtons; i++) {
             let button = document.createElement('button');
             button.appendChild(document.createTextNode("Choose me!"));
@@ -40,11 +49,45 @@ function playGame() {
             document.getElementById(i).onclick = checkWinner;
         }
     } else {
-        userInputError.appendChild(document.createTextNode('Please, enter a valide number from 1 to 10'));
+        userInputError.innerHTML = '';
+        if (name === '') {
+            userInputError.appendChild(document.createTextNode('Please, enter your name'));
+        } else {
+            userInputError.appendChild(document.createTextNode('Please, enter a valide number from 1 to 10'));
+        }
         userInputError.style.color = 'red';
-        setTimeout(function() {
-            location.reload();
-        }, 1000)
+        setTimeout( () => {
+            userInputError.innerHTML = '';
+        }, 1000);
     }
 }
 
+
+
+// modal
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
